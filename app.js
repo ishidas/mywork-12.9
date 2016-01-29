f2893ccc6e504c084f861988e47bb5d4ed14fdc9
Sawako Ishida <sawako.yoriko@gmail.com>
2016-01-29 00:07:23 -0800

    animation test feets moving left to right-creepy

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+(function(module){
+  var map = {};//map object
 
+  map.init = function () {
+  var gMap = new google.maps.Map(document.getElementById('#map'),{
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+  return gMap
+}
+
+  module.map = map;
+})(window);
